5477b47f1ecfaaa3e56f3e6d5e57ee278a1c184c
Anil <anilpujara125@gmail.com>
2021-10-23 12:50:27 +0545

    first commit

---
+++ myapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from myapp.models import Image
+
+# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    list_display=[ 'id','photo','date']
+
+admin.site.register(Image,ImageAdmin)
